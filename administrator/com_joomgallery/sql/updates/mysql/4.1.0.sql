bc57f1eea0b77e28972c8706c57c0f763fac1fe0
Elfangor <tech.spuur@quickline.ch>
2025-05-02 07:04:53 +0200

    add index-change

--- administrator/com_joomgallery/sql/updates/mysql/4.1.0.sql
+++ administrator/com_joomgallery/sql/updates/mysql/4.1.0.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `#__joomgallery_categories` ADD INDEX `idx_parent_id` (`parent_id`);
 ALTER TABLE `#__joomgallery_categories` ADD INDEX `idx_created_by` (`created_by`);
 ALTER TABLE `#__joomgallery_categories` ADD INDEX `idx_title` (`title`(191));
+ALTER TABLE `#__joomgallery_categories` DROP INDEX `idx_alias`, ADD INDEX `idx_alias` (`alias`(191));
+ALTER TABLE `#__joomgallery_categories` DROP INDEX `idx_path`, ADD INDEX `idx_path` (`path`(191));
