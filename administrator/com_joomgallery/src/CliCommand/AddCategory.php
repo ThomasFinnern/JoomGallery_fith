9eafcdbee5f46dd7bb53b9d15745d9acfdd208cc
ThomasFinnern <ThomasFinnernE5570@users.noreply.github.com>
2025-05-17 19:33:12 +0200

    add category input validatd

--- administrator/com_joomgallery/src/CliCommand/AddCategory.php
+++ administrator/com_joomgallery/src/CliCommand/AddCategory.php
@@ -3,16 +3,20 @@ namespace Joomgallery\Component\Joomgallery\Administrator\CliCommand;
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Factory\MVCFactoryAwareTrait;
-use Joomla\CMS\MVC\Model\DatabaseAwareTrait;
+use Joomla\Console\Command\AbstractCommand;
+use Joomla\Database\DatabaseAwareTrait;
+use Joomla\Database\DatabaseInterface;
 use Joomla\Filter\InputFilter;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class AddCategory extends \Joomla\Console\Command\AbstractCommand
+class AddCategory extends AbstractCommand
 {
   use MVCFactoryAwareTrait;
   use DatabaseAwareTrait;
@@ -58,13 +62,77 @@ class AddCategory extends \Joomla\Console\Command\AbstractCommand
 
     //--- assign option ----------------------------
 
-    $this->title = $input->getOption('title') ?? null;
-    $this->published = $input->getOption('published') ?? null;
-    $this->created_by = $input->getOption('created_by') ?? null;
-    $this->created_time = $input->getOption('created_time') ?? null;
-    $this->modified_by = $input->getOption('modified_by') ?? null;
-    $this->modified_time = $input->getOption('modified_time') ?? null;
-    $this->parent_title = $input->getOption('parent_title') ?? null;
+    // Get filter to remove invalid characters
+    $filter = new InputFilter();
+
+    // create/update time fallback
+    $date = Factory::getDate();
+    $actualTime = $date->toSql();
+
+    //--- title -----------------------
+
+    $this->title = $filter->clean($this->getStringFromOption(
+      'title', 'Please enter a category title'));
+
+    //--- created_by -----------------------
+
+    $this->created_by = $filter->clean($this->getStringFromOption(
+      'created_by', 'Please enter a username (owner)'));
+    $created_by_Id = $this->getUserId($this->created_by);
+
+    if (empty($created_by_Id)) {
+      $this->ioStyle->error("The user (owner)" . $this->created_by . " does not exist!");
+
+      return Command::FAILURE;
+    }
+
+    //--- modified_by -----------------------
+
+    $this->modified_by = $filter->clean($input->getOption('modified_by')) ?? null;
+
+    // not given by input use created by
+    if (empty($this->modified_by)) {
+      $this->modified_by = $this->created_by;
+      $this->modified_time = $created_by_Id;
+    } else {
+
+      $this->modified_by_Id = $this->getUserId($this->modified_by);
+
+      if (empty($modified_by_Id)) {
+        $this->ioStyle->error("The user (author)" . $this->modified_by . " does not exist!");
+
+        return Command::FAILURE;
+      }
+    }
+
+    $test = $filter->clean($input->getOption('published') ?? '0');
+    $this->published     = $filter->clean($input->getOption('published') ?? '0');
+    $this->created_time  = $filter->clean($input->getOption('created_time')  ?? $actualTime);
+    $this->modified_time = $filter->clean($input->getOption('modified_time') ?? $actualTime);
+    $this->parent_title  = $filter->clean($input->getOption('parent_title')  ?? 'root');
+
+    //--- validate -----------------------------------
+
+    if (!is_numeric($this->published)) {
+      $this->ioStyle->error('Invalid published value passed! (0/1) ? ');
+      return Command::FAILURE;
+    }
+
+
+    $category = [
+      'title' => $filter->clean($this->title, 'STRING'),
+      'published' => $filter->clean($this->published, 'INT'),
+      'created_by' => $filter->clean($this->created_by, 'STRING'),
+      'created_time' => $filter->clean($this->title, 'STRING'),
+      'modified_by' => $filter->clean($this->modified_by, 'STRING'),
+      'modified_time' => $filter->clean($this->title, 'STRING'),
+      'parent_title' => $filter->clean($this->parent_title, 'STRING'),
+    ];
+
+
+
+
+
 
 //    yyyy
 //
@@ -106,56 +174,101 @@ class AddCategory extends \Joomla\Console\Command\AbstractCommand
 ////      $filter->set('filter.created_by', $owner);
 //    }
 //
-
-    $categories = $categoriesModel->getItems();
-
-    // If no categories are found show a warning and set the exit code to 1.
-    if (empty($categories))
-    {
-      $this->ioStyle->warning('No categories found matching your criteria');
-
-      return 1;
-    }
-
-    // Reshape the categories into something humans can read.
-    $categories = array_map(
-      function (object $item): array
-      {
-        return [
-          $item->id,
-          $item->title,
-          $item->published ? Text::_('JYES') : Text::_('JNO'),
-          $item->created_by,
-          $item->created_time,
-          $item->modified_by,
-          $item->modified_time,
-          $item->parent_title, // JGLOBAL_ROOT
-          // $item->,
-
-        ];
-      },
-      $categories
-    );
-
-    // Display the categories in a table and set the exit code to 0
-    $this->ioStyle->table(
-      [
-//        Text::_('JGLOBAL_FIELD_ID_LABEL'),
-//        Text::_('JGLOBAL_TITLE'),
-//        Text::_('JPUBLISHED'),
-//        Text::_('JGLOBAL_FIELD_CREATED_BY_LABEL') . ' ('.  Text::_('COM_JOOMGALLERY_OWNER') . ')', // ToDo: Owner
-//        Text::_('JGLOBAL_FIELD_CREATED_LABEL'),
-//        Text::_('JGLOBAL_FIELD_MODIFIED_BY_LABEL'),
-//        Text::_('JGLOBAL_FIELD_MODIFIED_LABEL'),
-//        Text::_('JGLOBAL_LINK_PARENT_CATEGORY_LABEL'),
-        // Text::_(''),
-
-        'ID', 'Title', 'Published', 'Created by (owner)','Created','Modified by','Modified','Parent',
-      ],
-      $categories
-    );
+//
+//    $categories = $categoriesModel->getItems();
+//
+//    // If no categories are found show a warning and set the exit code to 1.
+//    if (empty($categories))
+//    {
+//      $this->ioStyle->warning('No categories found matching your criteria');
+//
+//      return 1;
+//    }
+//
+//    // Reshape the categories into something humans can read.
+//    $categories = array_map(
+//      function (object $item): array
+//      {
+//        return [
+//          $item->id,
+//          $item->title,
+//          $item->published ? Text::_('JYES') : Text::_('JNO'),
+//          $item->created_by,
+//          $item->created_time,
+//          $item->modified_by,
+//          $item->modified_time,
+//          $item->parent_title, // JGLOBAL_ROOT
+//          // $item->,
+//
+//        ];
+//      },
+//      $categories
+//    );
+//
+//    // Display the categories in a table and set the exit code to 0
+//    $this->ioStyle->table(
+//      [
+////        Text::_('JGLOBAL_FIELD_ID_LABEL'),
+////        Text::_('JGLOBAL_TITLE'),
+////        Text::_('JPUBLISHED'),
+////        Text::_('JGLOBAL_FIELD_CREATED_BY_LABEL') . ' ('.  Text::_('COM_JOOMGALLERY_OWNER') . ')', // ToDo: Owner
+////        Text::_('JGLOBAL_FIELD_CREATED_LABEL'),
+////        Text::_('JGLOBAL_FIELD_MODIFIED_BY_LABEL'),
+////        Text::_('JGLOBAL_FIELD_MODIFIED_LABEL'),
+////        Text::_('JGLOBAL_LINK_PARENT_CATEGORY_LABEL'),
+//        // Text::_(''),
+//
+//        'ID', 'Title', 'Published', 'Created by (owner)','Created','Modified by','Modified','Parent',
+//      ],
+//      $categories
+//    );
 
     return 0;
+  }
+
+  /**
+   * Method to get a value from option
+   *
+   * @param   string  $option    set the option name
+   *
+   * @param   string  $question  set the question if user enters no value to option
+   *
+   * @return  string
+   *
+   * @since   4.0.0
+   */
+  protected function getStringFromOption($option, $question): string
+  {
+    $answer = (string) $this->getApplication()->getConsoleInput()->getOption($option);
+
+    while (!$answer) {
+      $answer = (string) $this->ioStyle->ask($question);
+    }
+
+    return $answer;
+  }
+
+  /**
+   * Method to get a user object
+   *
+   * @param   string  $username  username
+   *
+   * @return  object
+   *
+   * @since   4.0.0
+   */
+  protected function getUserId($username)
+  {
+    // $db    = $this->getDatabase();
+    $db = Factory::getContainer()->get(DatabaseInterface::class);
+    $query = $db->getQuery(true)
+      ->select($db->quoteName('id'))
+      ->from($db->quoteName('#__users'))
+      ->where($db->quoteName('username') . '= :username')
+      ->bind(':username', $username);
+    $db->setQuery($query);
+
+    return $db->loadResult();
   }
 
   /**
@@ -171,12 +284,12 @@ class AddCategory extends \Joomla\Console\Command\AbstractCommand
 //    $this->addOption('search', 's', InputOption::VALUE_OPTIONAL, Text::_('COM_JOOMGALLERY_CLI_CONFIG_SEARCH'));
 
     $this->addOption('title', 't', InputOption::VALUE_REQUIRED, 'Title');
-    $this->addOption('published', null, InputOption::VALUE_OPTIONAL, 'Published (yes/no)'
-    $this->addOption('created_time', null, InputOption::VALUE_OPTIONAL, 'Created time'
-    $this->addOption('created_by', 'c', InputOption::VALUE_REQUIRED, 'Created by (owner)'
-    $this->addOption('modified_time', null, InputOption::VALUE_OPTIONAL, ''Modified time
-    $this->addOption('modified_by', 'm', InputOption::VALUE_OPTIONAL, 'Modified by'
-    $this->addOption('parent_title', 'p', InputOption::VALUE_OPTIONAL, 'parent title'
+    $this->addOption('published', null, InputOption::VALUE_OPTIONAL, 'Published (yes/no)');
+    $this->addOption('created_time', null, InputOption::VALUE_OPTIONAL, 'Created time');
+    $this->addOption('created_by', 'c', InputOption::VALUE_REQUIRED, 'Created by (owner)');
+    $this->addOption('modified_time', null, InputOption::VALUE_OPTIONAL, 'Modified time');
+    $this->addOption('modified_by', 'm', InputOption::VALUE_OPTIONAL, 'Modified by');
+    $this->addOption('parent_title', 'p', InputOption::VALUE_OPTIONAL, 'parent title');
 
     $help = "<info>%command.name%</info> will add a joomgallery category
 		    \nUsage: <info>php %command.full_name%</info>";
